fe6db9df155968f4e240aefc9f31e8be282642c9
Zoran Pandovski <zoran.pandovski@keitaro.com>
2017-07-11 01:19:48 +0200

    Initial tests

---
+++ ProdirectScraper/tests/test_helpers.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestConfigOptions(unittest.TestCase):
+    def test_configuration(self):
+        self.assertEqual(True, False)
+
+if __name__ == '__main__':
+    unittest.main()
