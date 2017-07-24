f99af13e3f7c5de783c67f3398ae86470a90e059
Zoran Pandovski <zoran.pandovski@keitaro.com>
2017-07-24 18:16:23 +0200

    Add tests

--- ProdirectScraper/tests/test_helpers.py
+++ ProdirectScraper/tests/test_helpers.py
@@ -1,8 +1,28 @@
 import unittest
+import helpers as h
 
 class TestConfigOptions(unittest.TestCase):
-    def test_configuration(self):
-        self.assertEqual(True, False)
+
+    def test_config_section_size_value(self):
+        #arrange
+
+        #act
+        config_size = h.config_section("trainers_spider")['size']
+        sizes = config_size.split(",")
+
+        #assert
+        for size in sizes:
+            self.assertTrue(4 <= int(size) <= 14)
+
+    def test_config_section_mailer(self):
+        #arrange
+        to = 'zoran.pandovski@gmail.com'
+
+        #act
+        config_to = h.config_section("mailer")['mail_to']
+
+        #assert
+        self.assertEqual(to, config_to)
 
 if __name__ == '__main__':
     unittest.main()
