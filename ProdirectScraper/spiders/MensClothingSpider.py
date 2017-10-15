8de78a607b2402dd02fb9e01e131a215ccfd595c
Filippo <github@filippo.pw>
2017-10-15 22:21:56 +0200

    Generalized Scraper

--- ProdirectScraper/spiders/MensClothingSpider.py
+++ ProdirectScraper/spiders/MensClothingSpider.py
@@ -1,47 +1,13 @@
-import scrapy
-from jinja2 import Template
-from ..emailer import send_mail
 from ..helpers import config_section
+from .PageScraper import PageSpider
 
 SIZE = config_section("mensclothing_spider")['size']
 CURRENCY = config_section("mensclothing_spider")['currency']
 
-class TrainersSpider(scrapy.Spider):
+
+class MensClothingSpider(PageSpider):
     name = 'mensclothing'
     home_url = 'http://www.prodirectselect.com/'
     start_urls = \
         [home_url + 'lists/mens-clothing.aspx?listName=mens-clothing&cur=' +
          CURRENCY + '&pp=32&pp=96&o=lth&s=' + SIZE]
-
-    def parse(self, response):
-        '''
-        Will be called to handle the response downloaded for each of the
-        requests made.
-        :param response:  holds the page content
-        :return:
-        '''
-        collection = []
-        for item in response.css('div.list div.item'):
-            trainers = {
-                'Price ': item.css('p.price::text').extract_first(),
-                'Description ': item.css('a::text').extract_first(),
-                'More info ': self.home_url +
-                              item.css('a::attr(href)').extract_first()
-            }
-            collection.append(trainers)
-
-        template = Template("""
-        <table>
-            {% for item in items %}
-                 <ul>
-                 {% for key, value in item.items() %}
-                    {% if value %}
-                       <li>{{value}}</li>
-                    {% endif %}
-                {% endfor %}
-                </ul>
-            {% endfor %}
-        </table>
-        """)
-        table = template.render(items=collection)
-        send_mail(table.encode('utf-8'))
---
+++ ProdirectScraper/spiders/PageScraper.py
@@ -0,0 +1,39 @@
+import scrapy
+from jinja2 import Template
+from ..emailer import send_mail
+
+
+class PageSpider(scrapy.Spider):
+
+    def parse(self, response):
+        '''
+        Will be called to handle the response downloaded for each of the
+        requests made.
+        :param response:  holds the page content
+        :return:
+        '''
+        collection = []
+        for item in response.css('div.list div.item'):
+            trainers = {
+                'Price ': item.css('p.price::text').extract_first(),
+                'Description ': item.css('a::text').extract_first(),
+                'More info ': self.home_url +
+                              item.css('a::attr(href)').extract_first()
+            }
+            collection.append(trainers)
+
+        template = Template("""
+        <table>
+            {% for item in items %}
+                 <ul>
+                 {% for key, value in item.items() %}
+                    {% if value %}
+                       <li>{{value}}</li>
+                    {% endif %}
+                {% endfor %}
+                </ul>
+            {% endfor %}
+        </table>
+        """)
+        table = template.render(items=collection)
+        send_mail(table.encode('utf-8'))
